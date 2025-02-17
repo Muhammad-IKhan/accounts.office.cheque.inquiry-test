2e2105b986c3bf83fcd755e181b2f6dafb356055
Muhammad Ismail Khan <muhammadismailk9@gmail.com>
2025-02-17 22:24:59 +0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -313,11 +313,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// Register service worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/accounts.office.cheque.inquiry-test/service-worker.js', { scope: '/accounts.office.cheque.inquiry-test/' })
-            .then(registration => console.log('ServiceWorker registered:', registration.scope))
-            .catch(err => console.error('ServiceWorker registration failed:', err));
-    });
-}
+// // Register service worker
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//         navigator.serviceWorker.register('/accounts.office.cheque.inquiry-test/service-worker.js', { scope: '/accounts.office.cheque.inquiry-test/' })
+//             .then(registration => console.log('ServiceWorker registered:', registration.scope))
+//             .catch(err => console.error('ServiceWorker registration failed:', err));
+//     });
+// }
